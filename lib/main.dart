947e237fd196d3814fd11d0fa1425c99461022ed
johnnylyimo <johnnylyimo@gmail.com>
2020-01-15 17:14:08 +0300

    Removed debug banner set to false

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class PianoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Piano App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
