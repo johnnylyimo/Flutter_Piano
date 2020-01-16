8a47e5337980a75f4fc02319104b95dd94ba82a8
johnnylyimo <johnnylyimo@gmail.com>
2020-01-16 12:43:47 +0300

    Created object textTheme from class TextTheme

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,8 @@ import 'package:flutter_piano/screens/splash_screen.dart';
 void main() => runApp(PianoApp());
 
 class PianoApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final TextTheme textTheme = TextTheme();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
